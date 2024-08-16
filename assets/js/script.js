2302d53715f14a8bf2ccf767c59ac01e933abd41
Syaddad Raihan Putra <89979969+SyaddadRaihanPutra@users.noreply.github.com>
2024-08-16 15:30:22 +0700

    Make Add/Edit Company and Change Structure Folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,29 @@ document.addEventListener("keydown", function (event) {
   // Cek apakah tombol `CTRL` dan `K` ditekan bersamaan
   if (event.ctrlKey && event.key === "k") {
     event.preventDefault(); // Mencegah tindakan default (seperti membuka browser bookmark)
-    document.getElementById("search").focus(); // Fokuskan elemen pencarian
+    var searchElement = document.getElementById("search");
+    if (searchElement) {
+      searchElement.focus(); // Fokuskan elemen pencarian
+    }
   }
 });
-document
-  .getElementById("toggle-password")
-  .addEventListener("click", function () {
+
+var togglePasswordButton = document.getElementById("toggle-password");
+if (togglePasswordButton) {
+  togglePasswordButton.addEventListener("click", function () {
     var passwordField = document.getElementById("password");
     var eyeIcon = document.getElementById("eye-icon");
 
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      eyeIcon.classList.remove("ti-eye");
-      eyeIcon.classList.add("ti-eye-off");
-    } else {
-      passwordField.type = "password";
-      eyeIcon.classList.remove("ti-eye-off");
-      eyeIcon.classList.add("ti-eye");
+    if (passwordField && eyeIcon) {
+      if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("ti-eye");
+        eyeIcon.classList.add("ti-eye-off");
+      } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("ti-eye-off");
+        eyeIcon.classList.add("ti-eye");
+      }
     }
   });
+}
